d3bb590aee89af6b7de220dd7b94efefca2a01ea
Patrick Corbett <patrick.corbett@live.com>
2025-04-21 16:08:44 +0200

    Section 3: HTTP Basic Auth Added Spring security test by creating and configuring a Web App Context. The test uses a generated authenticated user to test security logic i.e that an authenticated user is required to call this controller endpoint

---
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
@@ -0,0 +1,65 @@
+package guru.sfg.brewery.web.controllers;
+
+import guru.sfg.brewery.repositories.BeerInventoryRepository;
+import guru.sfg.brewery.repositories.BeerRepository;
+import guru.sfg.brewery.repositories.CustomerRepository;
+import guru.sfg.brewery.services.BeerService;
+import guru.sfg.brewery.services.BreweryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest
+public class BeerControllerIT {
+
+    // Spring security tests need Web MVC Filters to work so the web application context needs to be created
+    @Autowired
+    WebApplicationContext wac;
+
+    MockMvc mockMvc;
+
+    // As the Web App Context is being started, mock beans are required for ALL application controllers as the whole APP Context is being started
+    @MockBean
+    BeerRepository beerRepository;
+
+    @MockBean
+    BeerInventoryRepository beerInventoryRepository;
+
+    @MockBean
+    BreweryService breweryService;
+
+    @MockBean
+    CustomerRepository customerRepository;
+
+    @MockBean
+    BeerService beerService;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(wac) // provide a specific context
+                .apply(springSecurity()) // Activate spring security
+                .build();
+    }
+
+    @WithMockUser("spring")
+    // adds an authenticated user to spring context, this tests is not testing authentication but the security logic
+    @Test
+    void findBeers() throws Exception {
+        mockMvc.perform(get("/beers/find"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/findBeers"))
+                .andExpect(model().attributeExists("beer"));
+    }
+
+}
