553bd635eb6ae30fd27e66fa87cb60713d09a375
Patrick Corbett <patrick.corbett@live.com>
2025-04-21 23:35:04 +0200

    Section 4: Spring Security Java Configuration Video 24. Permit All with URL Pattern Matching Refactor Test classes to provide for allow anonymous or non authenticated requests access to the "/beers/find" and "/beers*" resources

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -14,7 +13,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests(authorize -> {
             // allow specific requests using path matching, these requests should be allowed without authentication
-            authorize.antMatchers("/", "/webjars/**", "/login", "/resources/**").permitAll();
+            authorize.antMatchers("/", "/webjars/**", "/login", "/resources/**", "/beers", "/beers/find").permitAll()
+                    /*
+                     * "/api/v1/user/*" - will match any value, up to another "/"
+                     * "/api/v1/user/**" - will match all values beginning with start of string (including if another "/" is found.
+                     */
+                    .antMatchers("/beers*", "/beers/find").permitAll();
         }).authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic();
     }
 }
--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
@@ -1,32 +1,18 @@
 package guru.sfg.brewery.web.controllers;
 
-import guru.sfg.brewery.repositories.BeerInventoryRepository;
-import guru.sfg.brewery.repositories.BeerRepository;
-import guru.sfg.brewery.repositories.CustomerRepository;
-import guru.sfg.brewery.services.BeerService;
-import guru.sfg.brewery.services.BreweryService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest
 public class BeerControllerIT extends BaseIT {
 
-    @WithMockUser("spring")
-    // adds an authenticated user to spring context, this tests is not testing authentication but the security logic
     @Test
     void findBeers() throws Exception {
+        // here no authentication is being provided
         mockMvc.perform(get("/beers/find"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("beers/findBeers"))
@@ -34,11 +20,15 @@ public class BeerControllerIT extends BaseIT {
     }
 
     @Test
-    void findBeersWithHttpBasic() throws Exception {
-        mockMvc.perform(get("/beers/find").with(httpBasic("spring", "guru"))) // Performs authentication, tests the Authentication and security logic
+    void findBeersWithAnonymous() throws Exception{
+        // spring security provides multiple authentication types, like HttpBasic before anonymous is a authentication type that allows unknown users access to resources
+        mockMvc.perform(get("/beers/find").with(anonymous()))
                 .andExpect(status().isOk())
                 .andExpect(view().name("beers/findBeers"))
                 .andExpect(model().attributeExists("beer"));
     }
+
+
+
 
 }
