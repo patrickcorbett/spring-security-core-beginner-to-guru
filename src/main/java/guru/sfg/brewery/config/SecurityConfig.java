ecc6eec0659b72a0ec2808078f44ba4afd91faa7
Patrick Corbett <patrick.corbett@live.com>
2025-04-22 19:49:35 +0200

    Section 5: In Memory Authentication Provider Video 30. In Memory Authentication Fluent API Replace the manual creation of the InMemoryUserDetailsManager as done via the UserDetailsService method to create the InMemoryUserDetailsManager using the Fluent API

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package guru.sfg.brewery.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -32,41 +33,42 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         }).authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic();
     }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // the AuthenticationManagerBuilder can also be used with it's Fluent API
+        // This allows for the definition and configuration of the InMemoryUserDetailsManager as before
+        auth.inMemoryAuthentication() // returns InMemoryUserDetailsManagerConfigurer
+                .withUser("spring")
+                .password("{noop}guru") // Specify the password encoder - noop - No Operation - Plain text
+                .roles("ADMIN")
+                .and()
+                .withUser("user")
+                .password("{noop}password") // Specify the password encoder - noop - No Operation - Plain text
+                .roles("USER");
+    }
+
     /*
      * Providing a BEan with the name userDetailsService overrides the default Spring boot Auto Config.
      * Here a new UserDetailsService with hard coded usernames and passwords is defined.
      * This creates essentially a HashMap of users for use in memory
      */
-    @Override
-    @Bean
-    protected UserDetailsService userDetailsService() {
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("spring")
-                .password("guru")
-                .roles("ADMIN")
-                .build();
-
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
-
-        return new InMemoryUserDetailsManager(admin, user);
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
+//    @Override
+//    @Bean
+//    protected UserDetailsService userDetailsService() {
+//        UserDetails admin = User.withDefaultPasswordEncoder()
+//                .username("spring")
+//                .password("guru")
+//                .roles("ADMIN")
+//                .build();
+//
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                .username("user")
+//                .password("password")
+//                .roles("USER")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(admin, user);
+//    }
 
 
 }
